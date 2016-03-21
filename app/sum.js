cea103bc7a6fdf020d6c0ea5ac60ba55bfaecdb3
Joey Arnstein and Jess Guilford <joeyarnstein@gmail.com>
2016-03-21 10:54:38 -0700

    create to do list functionality

---
+++ app/sum.js
@@ -0,0 +1,6 @@
+var number;
+var otherNumber;
+number = parseInt(prompt('enter a number'));
+otherNumber = parseInt(prompt('enter another one'));
+var sum = number + otherNumber;
+alert(sum);
---
+++ app/sum.ts
@@ -0,0 +1,7 @@
+var number: number;
+var otherNumber: number;
+
+number = parseInt(prompt('enter a number'));
+otherNumber = parseInt(prompt('enter another one'));
+var sum = number + otherNumber;
+alert(sum);
